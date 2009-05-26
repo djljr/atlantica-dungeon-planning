c2b4063166b3393415fafc90589146474390b61b
Den Lipovsky <dennis.lipovsky@gmail.com>
2009-05-26 13:00:28 +0000

    missing @Controller annotation

--- src/org/erenda/atlantica/DungeonRunning.java
+++ src/org/erenda/atlantica/DungeonRunning.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.erenda.atlantica.dao.CommonDao;
 import org.erenda.atlantica.dao.NationDungeonDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class DungeonRunning 
 {
 	@Autowired DungeonSetupOperations dungeonSetupOperations;
